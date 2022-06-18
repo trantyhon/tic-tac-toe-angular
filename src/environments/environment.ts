81bf0841ffc2952257a42bd8ac4d43e113dc439b
trantyhon <honkunzz11@gmail.com>
2022-06-18 10:03:55 +0700

    up2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  DefaultLanguage: "en",
+  production: false,
+  development: true,
+  environmentName: "DEV",
+  baseURL: "http://localhost:3000/api/" // your will be http://localhost/backend/api
 };
 
 /*
